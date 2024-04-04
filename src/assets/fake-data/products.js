b32c735dafc06fa7b65ab4e8095fad22b5a91e80
verkijikavictorine <victorineverkijika@gmail.com>
2024-04-04 21:23:04 +0100

    First commit

---
+++ src/assets/fake-data/products.js
@@ -0,0 +1,183 @@
+// all images are imported from images directory
+import product_01_image_01 from "../images/Ndole.jpg";
+import product_01_image_02 from "../images/ekwang.jpg";
+import product_01_image_03 from "../images/eru.jpg";
+
+import product_02_image_01 from "../images/kwacoco.png";
+import product_02_image_02 from "../images/eru.jpg";
+import product_02_image_03 from "../images/product_2.3.jpg";
+
+import product_03_image_01 from "../images/pepper-Soup.jpg";
+import product_03_image_02 from "../images/ekwang.jpg";
+import product_03_image_03 from "../images/product_3.3.jpg";
+
+import product_04_image_01 from "../images/achombo.jpeg";
+import product_04_image_02 from "../images/ekwang.jpg";
+import product_04_image_03 from "../images/product_4.3.png";
+
+import product_05_image_01 from "../images/banner.jpg";
+import product_05_image_02 from "../images/hot_Pot.jpeg";
+import product_05_image_03 from "../images/Ndole.jpg";
+
+import product_06_image_01 from "../images/bread(1).png";
+import product_06_image_02 from "../images/bread(2).png";
+import product_06_image_03 from "../images/bread(3).png";
+
+const products = [
+  {
+    id: "01",
+    title: "Ndole",
+    price: 2000.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: "Burger",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque. ",
+  },
+
+  {
+    id: "02",
+    title: "kwacoco",
+    price: 1500.0,
+    image01: product_02_image_01,
+    image02: product_02_image_02,
+    image03: product_02_image_03,
+    category: "Pizza",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "03",
+    title: "Pepper Soup" ,
+    price: 1500.0,
+    image01: product_03_image_01,
+    image02: product_03_image_02,
+    image03: product_03_image_03,
+    category: "Pizza",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "04",
+    title: "Achombo",
+    price: 1500.0,
+    image01: product_04_image_01,
+    image02: product_04_image_02,
+    image03: product_04_image_03,
+    category: "Pizza",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "05",
+    title: "Ndole",
+    price: 1500.0,
+    image01: product_05_image_01,
+    image02: product_05_image_02,
+    image03: product_05_image_03,
+    category: "Burger",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+  {
+    id: "06",
+    title: "Ndole",
+    price: 1500.0,
+    image01: product_01_image_01,
+    image02: product_01_image_02,
+    image03: product_01_image_03,
+    category: "Burger",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "07",
+    title: "Eru and Fufu",
+    price: 1500.0,
+    image01: product_02_image_02,
+    image02: product_02_image_01,
+    image03: product_02_image_03,
+    category: "Pizza",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "08",
+    title: "Ekwang",
+    price: 110.0,
+    image01: product_03_image_02,
+    image02: product_03_image_01,
+    image03: product_03_image_03,
+    category: "Pizza",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "09",
+    title: "Chicken schezwan rice",
+    price: 1500.0,
+    image01: product_04_image_02,
+    image02: product_04_image_01,
+    image03: product_04_image_03,
+    category: "Pizza",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "10",
+    title: "Porish Irish",
+    price: 24.0,
+    image01: product_05_image_02,
+    image02: product_05_image_01,
+    image03: product_05_image_03,
+    category: "Burger",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "11",
+    title: "Crunchy Bread ",
+    price: 35.0,
+    image01: product_06_image_01,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "12",
+    title: "Delicious Bread ",
+    price: 35.0,
+    image01: product_06_image_02,
+    image02: product_06_image_01,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "13",
+    title: "Loaf Bread ",
+    price: 35.0,
+    image01: product_06_image_03,
+    image02: product_06_image_02,
+    image03: product_06_image_03,
+    category: "Bread",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+];
+
+export default products;
